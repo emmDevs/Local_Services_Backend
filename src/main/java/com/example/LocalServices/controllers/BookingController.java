b9b8dadf1b6427df448aa4402032a975fae56559
Emma Hack <ehack78@gmail.com>
2021-01-30 19:17:43 +0000

    GET route set up for booking controller

---
+++ src/main/java/com/example/LocalServices/controllers/BookingController.java
@@ -0,0 +1,39 @@
+package com.example.LocalServices.controllers;
+
+import com.example.LocalServices.models.Booking;
+import com.example.LocalServices.repositories.BookingRepository;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RestController
+public class BookingController {
+
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void configurePathMatch(PathMatchConfigurer configurer) {
+            AntPathMatcher matcher = new AntPathMatcher();
+            matcher.setCaseSensitive(false);
+            configurer.setPathMatcher(matcher);
+        }
+    }
+
+    @Autowired
+    BookingRepository bookingRepository;
+
+    @GetMapping(value = "/bookings")
+    public ResponseEntity<List<Booking>> getAllBookings(){
+        return new ResponseEntity(bookingRepository.findAll(), HttpStatus.OK);
+    }
+}
