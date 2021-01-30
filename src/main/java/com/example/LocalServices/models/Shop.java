077f3a1f576d3aedda489705cda5ecc49f3ce563
Emma Hack <ehack78@gmail.com>
2021-01-30 16:04:12 +0000

    shop initial class set up

--- src/main/java/com/example/LocalServices/models/Shop.java
+++ src/main/java/com/example/LocalServices/models/Shop.java
@@ -48,13 +48,13 @@ public class Shop {
     private List<Service> services;
 
     @ManyToMany
-    @JsonIgnoreProperties({"service"})
+    @JsonIgnoreProperties({"shop"})
     @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
     @JoinTable(
-            name = "services_categories",
+            name = "shops_categories",
             joinColumns = {
                     @JoinColumn(
-                            name="service_id",
+                            name="shop_id",
                             nullable = false,
                             updatable = false
                     )
@@ -69,9 +69,115 @@ public class Shop {
     )
     private List<Category> categories;
 
+    public Shop(){
 
+    }
 
+    public Shop(String name, String address, String postcode, String town, Time opening_hour, Time closing_hour, int telephone_number, String email, String image){
+        this.name = name;
+        this.address = address;
+        this.postcode = postcode;
+        this.town = town;
+        this.opening_hour = opening_hour;
+        this.closing_hour = closing_hour;
+        this.telephone_number = telephone_number;
+        this.email = email;
+        this.image = image;
+    }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public Time getOpening_hour() {
+        return opening_hour;
+    }
+
+    public void setOpening_hour(Time opening_hour) {
+        this.opening_hour = opening_hour;
+    }
+
+    public Time getClosing_hour() {
+        return closing_hour;
+    }
+
+    public void setClosing_hour(Time closing_hour) {
+        this.closing_hour = closing_hour;
+    }
+
+    public int getTelephone_number() {
+        return telephone_number;
+    }
+
+    public void setTelephone_number(int telephone_number) {
+        this.telephone_number = telephone_number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public List<Service> getServices() {
+        return services;
+    }
+
+    public void setServices(List<Service> services) {
+        this.services = services;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
 }
