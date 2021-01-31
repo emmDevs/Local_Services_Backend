59d6bcd74cbe571521c2a3eec5337b851cd399d2
Emma Hack <ehack78@gmail.com>
2021-01-31 11:17:46 +0000

    dataLoader complete with initial data

--- src/main/java/com/example/LocalServices/components/DataLoader.java
+++ src/main/java/com/example/LocalServices/components/DataLoader.java
@@ -76,14 +76,57 @@ public class DataLoader implements ApplicationRunner {
         Booking booking1 = new Booking("2021-01-25", "2021-01-30", 1030, 1130, "1 pair of brown leather boots to be re- heeled and re-soled please", user1);
         bookingRepository.save(booking1);
 
-        Booking booking2 = new Booking("2021-01-05", "2021-02-06", 1330, 1430, "facial for dry skin please", user2);
+        Booking booking2 = new Booking("2021-01-05", "2021-02-06", 1300, 1430, "facial for dry skin and a luxury manicure please", user2);
         bookingRepository.save(booking2);
 
-        Booking booking3 = new Booking("2020-12-29", "2021-01-15", 1200, 1230, "french manicure", user1);
+        Booking booking3 = new Booking("2020-12-29", "2021-01-15", 1200, 1330, "french manicure", user1);
         bookingRepository.save(booking3);
 
-        Booking booking4 = new Booking("2021-01-05", "2021-02-06", 1300, 1330, "luxury manicure", user2);
-        bookingRepository.save(booking4);
+        shop1.addService(service1);
+        shop1.addCategory(category1);
+        shopRepository.save(shop1);
+
+        shop2.addService(service2);
+        shop2.addService(service3);
+        shop2.addCategory(category2);
+        shopRepository.save(shop2);
+
+        service1.addSlots(slot1);
+        service1.addSlots(slot2);
+        service1.addSlots(slot3);
+        service1.addSlots(slot4);
+        serviceRepository.save(service1);
+
+        service2.addSlots(slot1);
+        service2.addSlots(slot2);
+        service2.addSlots(slot3);
+        service2.addSlots(slot4);
+        serviceRepository.save(service2);
+
+        service3.addSlots(slot1);
+        service3.addSlots(slot2);
+        service3.addSlots(slot3);
+        service3.addSlots(slot4);
+        serviceRepository.save(service3);
+
+        booking1.addService(service1);
+        bookingRepository.save(booking1);
+
+        booking2.addService(service2);
+        booking2.addService(service3);
+        bookingRepository.save(booking2);
+
+        booking3.addService(service3);
+        bookingRepository.save(booking3);
+
+        user1.addBooking(booking1);
+        user1.addBooking(booking3);
+        userRepository.save(user1);
+
+        user2.addBooking(booking2);
+        userRepository.save(user2);
+
+
 
 
 
