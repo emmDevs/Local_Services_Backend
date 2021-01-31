56f46b604aa8b7414f2c13f8146b7e92585b5726
Mateusz <mateusz.harlos@gmail.com>
2021-01-31 11:44:52 +0000

    updating shop

--- src/main/frontend/src/components/Shop.js
+++ src/main/frontend/src/components/Shop.js
@@ -3,32 +3,45 @@ import axios from 'axios';
 import NewShopForm from "./NewShopForm";
 
 
-const ShopProfiles = () => {
-    const fetchShopProfiles = () => {
-      axios.get("http://localhost:8080/shops")
-      .then(res => {
-        console.log(res)
-      })
-    }
-  
-    useEffect(() => {
-      fetchShopProfiles();
-    }, []);
-  
-    return <p>Fetch Test</p>
-  }
 
 function Shop() {
 
+  const [shopProfileList, setShopProfileList] = useState([]);
+  const [shopProfile, setShopProfile] = useState([]);
 
-    return(
-        <div>
-            <h3>Shop Component</h3>
-            <h4> Create New Profile or Log In</h4>
-            <ShopProfiles/>
-            <NewShopForm />
-        </div>
-    )
+  useEffect(() => {
+    axios.get('http://localhost:8080/shops/')
+      .then(res => res.json())
+      .then((data) => {
+        const promises = data.map((id))
+      });
+  })
+
+  // const ShopProfiles = () => {
+  //   const [shopProfile, setShopProfile] = useState([]);
+  
+  //   const fetchShopProfiles = () => {
+  //     axios.get('https://jsonplaceholder.typicode.com/users/')
+  //     .then(res => {
+  //       console.log(res);
+  //     });
+  //   }
+  
+  //   useEffect(() => {
+  //     fetchShopProfiles();
+  //   }, []);
+  
+  //   return <p>Fetch Test</p>
+  //   }
+
+  return(
+    <div>
+        <h3>Shop Component</h3>
+        <h4> Create New Profile or Log In</h4>
+        {/* <ShopProfiles /> */}
+        <NewShopForm />
+    </div>
+  )
 }
 
 export default Shop;
