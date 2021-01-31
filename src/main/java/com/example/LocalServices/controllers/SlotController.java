12e59d66ffbb364cd82d6c40de05946fde13608a
Emma Hack <ehack78@gmail.com>
2021-01-31 19:25:28 +0000

    no changes made but git commit required

--- src/main/java/com/example/LocalServices/controllers/SlotController.java
+++ src/main/java/com/example/LocalServices/controllers/SlotController.java
@@ -1,6 +1,7 @@
 package com.example.LocalServices.controllers;
 
 import com.example.LocalServices.models.Slot;
+import com.example.LocalServices.models.User;
 import com.example.LocalServices.repositories.SlotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
