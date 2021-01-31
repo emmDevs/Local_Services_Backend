7ba28cde6c4e6dfe088a49b92bb94118de5b7186
Emma Hack <ehack78@gmail.com>
2021-01-31 09:36:42 +0000

    opening and closing hours in shop model changed from time to string

--- src/main/java/com/example/LocalServices/components/DataLoader.java
+++ src/main/java/com/example/LocalServices/components/DataLoader.java
@@ -1,6 +1,7 @@
 package com.example.LocalServices.components;
 
 import com.example.LocalServices.models.Category;
+import com.example.LocalServices.models.Shop;
 import com.example.LocalServices.models.Slot;
 import com.example.LocalServices.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,7 +42,23 @@ public class DataLoader implements ApplicationRunner {
         Category category2 = new Category("Beauty", "Pamper yourself", "https://pixabay.com/illustrations/massage-este-relax-relaxation-spa-1237913/");
         categoryRepository.save(category2);
 
-        Slot slot1 = new Slot(090, 100);
+       Slot slot1 = new Slot(900, 1000);
+       slotRepository.save(slot1);
+
+       Slot slot2 = new Slot(1030, 1130);
+       slotRepository.save(slot2);
+
+       Slot slot3 = new Slot(1200, 1300);
+       slotRepository.save(slot3);
+
+       Slot slot4 = new Slot(1330, 1430);
+       slotRepository.save(slot4);
+
+        Shop shop1 = new Shop("Claire the Cobbler", "123 High Street", "EH42 1FA", "Dunbar", )
+
+
+
+
 
 
 
