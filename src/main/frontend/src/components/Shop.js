84efd6c34b11a5ef5d74e49cb5190611104e3144
Mateusz <mateusz.harlos@gmail.com>
2021-01-31 22:03:29 +0000

    shop - navigation

--- src/main/frontend/src/components/Shop.js
+++ src/main/frontend/src/components/Shop.js
@@ -1,41 +1,81 @@
 import React, {useState, useEffect} from 'react';
+import { Link } from "react-router-dom";
 import axios from 'axios';
 import NewShopForm from "./NewShopForm";
-import ShopDetails from "./ShopDetails";
+// import ShopDetails from "./ShopDetails";
 
 
 
-function Shop() {
+const Shop = () => {
 
-  const [shopProfile, setShopProfile] = useState({});
-  const [selectedShopId, setSelectedShopId] = useState({});
-  const [load, setLoad] = useState(false);
+  const [shopList, setShopList] = useState([]);
+
+  // const [shopProfile, setShopProfile] = useState({});
+  // const [selectedShopId, setSelectedShopId] = useState({});
+  
+
+  // useEffect(() => {
+  //   getShopProfile();
+  // }, [selectedShopId]);
+
+  const getShopList = () => {
+    axios.get(`http://localhost:8080/shops/`)
+    .then(res => {
+      console.log(res);
+      setShopList(res.data)
+    });
+  };
 
   useEffect(() => {
-    getShopProfile();
-  }, [selectedShopId]);
+    getShopList();
+  }, []);
 
-  const getShopProfile = () => {
-    console.log("getting shop data");
+  // const displayShopForm = () => {
+  //   setNewForm(true);
+  //   }
+  
+  
 
-    fetch(`http://localhost:8080/shops/${selectedShopId}`)
-      .then(res => res.json())
-      .then(data => setShopProfile(data))
-      .then(() => setLoad(true))
-      
-  }
-
-  return(
+  return (
     <div>
-        <h3>Shop Component</h3>
-        <h4> Create New Profile or Log In</h4>
-        <ShopDetails 
-        shopProfile={shopProfile}
-        load={load}
-        />
-        <NewShopForm />
+      <h3>Shop Component</h3>
+      {shopList
+        ? shopList.map((shop, index) => {
+          return(
+            <div key={index}>
+              <h5>Id {shop.id}. {shop.name}</h5>
+            </div>
+          );
+        })
+        : "Loading..."}
+        <Link to="/new-form"><button className="btn">Create New Profile</button></Link>
     </div>
-  )
+  );
+  
+
+  // const getShopProfile = () => {
+  //   console.log("getting shop data");
+
+  //   fetch(`http://localhost:8080/shops/${selectedShopId}`)
+  //     .then(res => res.json())
+  //     .then(data => setShopProfile(data))
+  //     .then(() => setLoad(true))
+  // }
+  
+
+  // return(
+  //   <div>
+  //       <h3>Shop Component</h3>
+  //       <h4> Create New Profile or Log In</h4>
+  //       <h4></h4>
+        
+  //       {/* <ShopDetails 
+  //       shopProfile={shopProfile}
+  //       load={load}
+  //       /> */}
+  //       <NewShopForm />
+  //   </div>
+  // )
 }
 
 export default Shop;
