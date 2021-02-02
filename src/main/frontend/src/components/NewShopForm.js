ca75394bc8db523069cef0784a805841f1e77f6a
Mateusz <mateusz.harlos@gmail.com>
2021-02-02 12:01:30 +0000

    updating input type to fit backend

--- src/main/frontend/src/components/NewShopForm.js
+++ src/main/frontend/src/components/NewShopForm.js
@@ -1,11 +1,11 @@
 import React, {useState} from "react";
-// import axios from "axios";
+
 
 function NewShopForm() {
     const [formData, setFormData] = useState({
         name: '',
         address: '',
-        postCode: '',
+        postcode: '',
         town: '',
         openingHour: '',
         closingHour: '',
@@ -35,9 +35,7 @@ function NewShopForm() {
               'Content-Type': 'application/json'
             }
         })
-        // const request = new Request();
-        // request.post("http://localhost:8080/shops/", formData)
-        // .then(() => window.location = '/shops')
+        .then(() => window.location = "/shop")
       }
 
     return(
@@ -69,14 +67,14 @@ function NewShopForm() {
                 </div>
 
                 <div className="form_wrap">
-                    <label htmlFor="postCode">Post Code:</label>
+                    <label htmlFor="postcode">Postcode:</label>
                     <input
                     onChange={handleChange}
                     type="text"
-                    name="postCode"
-                    id="postCode"
-                    placeholder="Post Code"
-                    value={formData.postCode}
+                    name="postcode"
+                    id="postcode"
+                    placeholder="Postcode"
+                    value={formData.postcode}
                     required/>
                 </div>
 
@@ -118,7 +116,7 @@ function NewShopForm() {
                     <label htmlFor="telephoneNumber">Telephone Number:</label>
                     <input
                     onChange={handleChange}
-                    type="number"
+                    type="text"
                     name="telephoneNumber"
                     id="telephoneNumber"
                     placeholder="Telephone Number"
@@ -179,174 +177,6 @@ function NewShopForm() {
         </div>
     );
 
-    // const [name, setName] = useState("");
-    // const [address, setAddress] = useState("");
-    // const [postCode, setPostCode] = useState("");
-    // const [town, setTown] = useState("");
-    // const [openingHour, setOpeningHour] = useState("");
-    // const [closingHour, setClosingHour] = useState("");
-    // const [telephoneNumber, setTelephoneNumber] = useState("");
-    // const [email, setEmail] = useState("");
-    // const [image, setImage] = useState("");
-    // const [categories, setCategories] = useState("");
-    // const [services, setServices] = useState("");
-
-    // const handleNameChange = (evt) => {
-    //     setName(evt.target.value);
-    // }
-
-    // const handleAddressChange = (evt) => {
-    //     setAddress(evt.target.value);
-    // }
-
-    // const handlePostCodeChange = (evt) => {
-    //     setPostCode(evt.target.value);
-    // }
-
-    // const handleTownChange = (evt) => {
-    //     setTown(evt.target.value);
-    // }
-
-    // const handleOpeningHourChange = (evt) => {
-    //     setOpeningHour(evt.target.value);
-    // }
-
-    // const handleClosingHourChange = (evt) => {
-    //     setClosingHour(evt.target.value);
-    // }
-
-    // const handleTelephoneNumberChange = (evt) => {
-    //     setTelephoneNumber(evt.target.value);
-    // }
-
-    // const handleEmailChange = (evt) => {
-    //     setEmail(evt.target.value);
-    // }
-
-    // const handleImageChange = (evt) => {
-    //     setImage(evt.target.value);
-    // }
-
-    // const handleCategoriesChange = (evt) => {
-    //     setCategories(evt.target.value);
-    // }
-
-    // const handleServicesChange = (evt) => {
-    //     setServices(evt.target.value);        
-    // }
-
-    // const handleSubmit = (evt) => {
-    //     evt.preventDefault();
-    //     const nameToSubmit = name.trim();
-    //     const addressToSubmit = address.trim();
-    //     const postCodeToSubmit = postCode.trim();
-    //     const townToSubmit = town.trim();
-    //     const openingHourToSubmit = openingHour.trim();
-    //     const closingHourToSubmit = closingHour.trim();
-    //     const telephoneNumberToSubmit = telephoneNumber.trim();
-    //     const emailToSubmit = email.trim();
-    //     const imageToSubmit = image.trim();
-    //     const categoriesToSubmit = categories.trim();
-    //     const servicesToSubmit = services.trim();
-
-    //     if(!nameToSubmit || !addressToSubmit || !postCodeToSubmit 
-    //         || !townToSubmit || !openingHourToSubmit || !closingHourToSubmit 
-    //         || !telephoneNumberToSubmit || !emailToSubmit || !imageToSubmit 
-    //         || !categoriesToSubmit || !servicesToSubmit) {
-    //             return;
-    //         }
-    //         onFormSubmit({
-    //             name: nameToSubmit,
-    //             address: addressToSubmit,
-    //             postCode: postCodeToSubmit,
-    //             town: townToSubmit,
-    //             openingHour: openingHourToSubmit,
-    //             closingHour: closingHourToSubmit,
-    //             telephoneNumber: telephoneNumberToSubmit,
-    //             email: emailToSubmit,
-    //             image: imageToSubmit,
-    //             categories: categoriesToSubmit,
-    //             services: servicesToSubmit
-    //         })
-    //         setName("");
-    //         setAddress("");
-    //         setPostCode("");
-    //         setTown("");
-    //         setOpeningHour("");
-    //         setClosingHour("");
-    //         setTelephoneNumber("");
-    //         setEmail("");
-    //         setImage("");
-    //         setCategories("");
-    //         setServices("");
-    // }
-
-    // return(
-    //     <div>
-    //         <h3>Create New Profile</h3>
-    //         <form className="new-shop-form" onSubmit={handleSubmit}>
-
-    //             <input type="text"
-    //             placeholder="Name"
-    //             value={name}
-    //             onChange={handleNameChange}/>
-
-    //             <input type="text"
-    //             placeholder="Address"
-    //             value={address}
-    //             onChange={handleAddressChange}/>
-
-    //             <input type="text"
-    //             placeholder="Post Code"
-    //             value={postCode}
-    //             onChange={handlePostCodeChange}/>
-
-    //             <input type="text"
-    //             placeholder="Town"
-    //             value={town}
-    //             onChange={handleTownChange}/>
-
-    //             <label>Opening Hour</label>
-    //             <input type="time"
-    //             value={openingHour}
-    //             onChange={handleOpeningHourChange}/>
-
-    //             <label>Closing Hour</label>
-    //             <input type="time"
-    //             value={closingHour}
-    //             onChange={handleClosingHourChange}/>
-
-    //             <input type="number"
-    //             placeholder="Telephone Number"
-    //             value={telephoneNumber}
-    //             onChange={handleTelephoneNumberChange}/>
-
-    //             <input type="text"
-    //             placeholder="Email"
-    //             value={email}
-    //             onChange={handleEmailChange}/>
-
-    //             <input type="text"
-    //             placeholder="Image"
-    //             value={image}
-    //             onChange={handleImageChange}/>
-
-    //             <input type="text"
-    //             placeholder="Categories"
-    //             value={categories}
-    //             onChange={handleCategoriesChange}/>
-
-    //             <input type="text"
-    //             placeholder="Services"
-    //             value={services}
-    //             onChange={handleServicesChange}/>
-
-    //             <input type="submit"
-    //             value="submit"/>
-    //         </form>
-            
-    //     </div>
-    // )
 }
 
 export default NewShopForm;
