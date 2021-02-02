43e482796e0afb856f4eda9ea1723a32d0226dc6
Mateusz <mateusz.harlos@gmail.com>
2021-02-02 13:39:37 +0000

    shop details updated

--- src/main/frontend/src/components/ShopDetails.js
+++ src/main/frontend/src/components/ShopDetails.js
@@ -24,9 +24,9 @@ const ShopDetails = () => {
             <p>{shop.address}</p>
             <p>{shop.postcode}</p>
             <p>{shop.town}</p>
-            <p>{shop.opening_hour}</p>
-            <p>{shop.closing_hour}</p>
-            <p>{shop.telephone_number}</p>
+            <p>{shop.openingHour}</p>
+            <p>{shop.closingHour}</p>
+            <p>{shop.telephoneNumber}</p>
             <p>{shop.email}</p>  
             <img src={shop.image} alt="no available"/>
             {/* <p>{shop.services[0].name}</p>
